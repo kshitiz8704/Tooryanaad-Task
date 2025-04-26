ad744aef44a10a8d04c4c38c1d6a285fc4dcd774
kshitiz8704 <kshitizdeep912@gmail.com>
2025-04-26 12:33:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const checkbtn = document.querySelector(".checkbtn");
 const list = document.querySelector(".list");
+const hero = document.querySelector(".hero");
+const skills = document.querySelector(".skills");
+const workProcess = document.querySelector(".work-process");
+const newsletter = document.querySelector(".newsletter");
+const about = document.querySelector(".aboutbtn");
+const home = document.querySelector(".home");
+const home1 = document.querySelector(".home1");
+const about1 = document.querySelector(".aboutbtn1");
+const home2 = document.querySelector(".home2");
+const about2 = document.querySelector(".aboutbtn2");
 
 
 
@@ -13,4 +23,46 @@ checkbtn.addEventListener("click", (event) => {
         list.classList.remove('display');
         clickCount = 0;
     }
+});
+
+about.addEventListener("click", (event) => {
+    hero.classList.add('hidden');
+    skills.classList.add('hidden');
+    workProcess.classList.add('hidden');
+    newsletter.classList.add('hidden');
+});
+
+about1.addEventListener("click", (event) => {
+    hero.classList.add('hidden');
+    skills.classList.add('hidden');
+    workProcess.classList.add('hidden');
+    newsletter.classList.add('hidden');
+});
+
+about2.addEventListener("click", (event) => {
+    hero.classList.add('hidden');
+    skills.classList.add('hidden');
+    workProcess.classList.add('hidden');
+    newsletter.classList.add('hidden');
+});
+
+home.addEventListener("click", (event) => {
+    hero.classList.remove('hidden');
+    skills.classList.remove('hidden');
+    workProcess.classList.remove('hidden');
+    newsletter.classList.remove('hidden');
+});
+
+home1.addEventListener("click", (event) => {
+    hero.classList.remove('hidden');
+    skills.classList.remove('hidden');
+    workProcess.classList.remove('hidden');
+    newsletter.classList.remove('hidden');
+});
+
+home2.addEventListener("click", (event) => {
+    hero.classList.remove('hidden');
+    skills.classList.remove('hidden');
+    workProcess.classList.remove('hidden');
+    newsletter.classList.remove('hidden');
 });
